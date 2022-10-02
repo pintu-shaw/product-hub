102818c5bb0441a6fed9349b84e06fbcc1d9cde5
Pintu Shaw <pintu.autopilotgeek@gmail.com>
2022-10-02 21:28:26 +0530

    Add seeds for product and user.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email: "user@test.com", password: "password")
+Product.create(title: "MacBook Air M1", short_description: "A short description.", description: "A very long description.", original_price: 300000, final_price: 270000)
