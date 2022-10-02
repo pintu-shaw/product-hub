751ce5ea9e3cfc59d30d25e238a01d43d584036f
Pintu Shaw <pintu.autopilotgeek@gmail.com>
2022-10-02 20:52:37 +0530

    Remove comments from products_controller.rb. Add before_action authenticate_user for specified actions in products_controller.rb.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,25 +1,21 @@
 class ProductsController < ApplicationController
+  before_action :authenticate_user!, only: %i[ create edit update destroy ]
   before_action :set_product, only: %i[ show edit update destroy ]
 
-  # GET /products or /products.json
   def index
     @products = Product.all
   end
 
-  # GET /products/1 or /products/1.json
   def show
   end
 
-  # GET /products/new
   def new
     @product = Product.new
   end
 
-  # GET /products/1/edit
   def edit
   end
 
-  # POST /products or /products.json
   def create
     @product = Product.new(product_params)
 
@@ -34,7 +30,6 @@ class ProductsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /products/1 or /products/1.json
   def update
     respond_to do |format|
       if @product.update(product_params)
@@ -47,7 +42,6 @@ class ProductsController < ApplicationController
     end
   end
 
-  # DELETE /products/1 or /products/1.json
   def destroy
     @product.destroy
 
@@ -58,12 +52,12 @@ class ProductsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_product
       @product = Product.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
+
     def product_params
       params.require(:product).permit(:title, :short_description, :description, :original_price, :final_price)
     end
